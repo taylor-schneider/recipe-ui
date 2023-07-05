0502d8d8a16f4111bc60bc8824de0c77abacb93d
Schneider <taylor.schneider@accenture.com>
2023-07-05 18:28:13 -0500

    Started working with the router.

--- src/Pages/Recipe/Recipe.js
+++ src/Pages/Recipe/Recipe.js
@@ -1,18 +1,34 @@
 import './Recipe.css';
-import Title from './../../Components/Title/Title'
-import Version from './../../Components/Version/Version'
+import Title from '../../Components/Title/Title'
+import Version from '../../Components/Version/Version'
 import React, { Component } from "react";
+import {useLocation, useNavigate, useParams } from "react-router-dom";
+import { useSearchParams } from 'react-router-dom';
 
-export default class Recipe extends Component {
+class Recipe extends Component {
 
   state = {
-    guid: "",
     recipe: {}
   };
 
   componentDidMount() {
-    const recipe_url = "http://127.0.0.1:8080/recipe/" + this.props.guid;
 
+    console.log("These are the props:")
+    console.log(this.props)
+    const search_string = this.props.router.location.search
+    console.log("search string: ")
+    console.log(search_string)
+    const queryParams = new URLSearchParams(search_string)
+    const guid = queryParams.get("guid")
+    const version = queryParams.get("version")
+
+    var recipe_url = "http://127.0.0.1:8080/recipe?guid=" + guid;
+    if (version != null){
+      recipe_url += "&version=" + version
+    }
+
+    console.log("recipe url")
+    console.log(recipe_url)
     fetch(recipe_url)
       .then(res => res.json())
       .then((recipe) => {
@@ -21,14 +37,33 @@ export default class Recipe extends Component {
   }
 
   render() {
-    console.log(this.state.recipe)
+
+    console.log("render:")
+    console.log(this.state)
 
     return (
-      <div id={this.props.guid} className="recipe">
-        <h1>{this.props.guid}</h1>
+      <div id={this.state.guid} className="recipe">
         {Title(this.state.recipe.name)}
         {Version(this.state.recipe.version)}
       </div>
     );
   }
 }
+
+function withRouter(Component) {
+  function ComponentWithRouterProp(props) {
+    let location = useLocation();
+    let navigate = useNavigate();
+    let params = useParams();
+    return (
+      <Component
+        {...props}
+        router={{ location, navigate, params }}
+      />
+    );
+  }
+
+  return ComponentWithRouterProp;
+}
+
+export default withRouter(Recipe)
--- src/index.js
+++ src/index.js
@@ -1,20 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import Home from './Pages/Home/Home';
 import Recipe from './Pages/Recipe/Recipe';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter, Routes, Route, Link} from 'react-router-dom';
 
-var recipes_url = "http://127.0.0.1:8080/recipes"
-let response = await fetch(recipes_url);
-let recipe_guids = await response.json();
-var recipe_guid = recipe_guids[0];
+
+
+export default function App() {
+  return (
+    
+    <BrowserRouter>
+      <Link to="/recipe?guid=foobar">Foobar Recipe</Link>
+      <Routes>
+        <Route exact path='/' element={< Home />}></Route>
+        <Route exact path='/recipe' element={<Recipe />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <Recipe guid = {recipe_guid}/>
-  </React.StrictMode>
-);
+root.render(<App />);
+
+//var recipes_url = "http://127.0.0.1:8080/recipes"
+//let response = await fetch(recipes_url);
+//let recipe_guids = await response.json();
+//var recipe_guid = recipe_guids[0];
+//
+//const root = ReactDOM.createRoot(document.getElementById('root'));
+//root.render(
+//  <React.StrictMode>
+//    <Recipe guid = {recipe_guid}/>
+//  </React.StrictMode>
+//);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
