e4b22fba6caf7e35561450918a102bcddff7eaaa
Schneider <taylor.schneider@accenture.com>
2023-07-11 07:21:42 -0500

    Adding example test

---
+++ tests/Pages/Recipe/Recipe.test.js
@@ -0,0 +1,7 @@
+import Recipe from '../../../src/Pages/Recipe/Recipe'
+
+describe("Calculator tests", () => {
+    test('adding 1 + 2 should return 3', () => {
+        console.log("hello world!")
+    });
+   })
